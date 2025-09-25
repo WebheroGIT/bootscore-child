3113eff2bb168b3c1f3b38d83354aa806e5080b5
ePiliK <lucadantonio@gmail.com>
2025-09-25 14:34:17 +0000

    feat: aggiorna logica PHP principale

--- _dev/inc/logic/logic.php
+++ _dev/inc/logic/logic.php
@@ -50,7 +50,7 @@ require_once get_stylesheet_directory(). '/_dev/inc/logic/plugin/plugins.php';
 // Video Modal utility include
 require_once get_stylesheet_directory(). '/_dev/inc/logic/utility/video-modal.php';
 // struttura post template include
-require_once get_stylesheet_directory(). '/_dev/inc/logic/utility/struttura-post-templatephp';
+require_once get_stylesheet_directory(). '/_dev/inc/logic/utility/struttura-post-template.php';
 
 // Video Modal utility include
 require_once get_stylesheet_directory(). '/_dev/assets-admin/admin-search.php';
